0263172e4586f56cb8eda4d2e74a802d08219b78
Do Thanh Tuan <81170150+tuanit1@users.noreply.github.com>
2022-05-06 14:51:52 +0700

    tuan push

---
+++ app/src/main/java/com/naosteam/watchvideoapp/fragments/VideoDetailFragment.java
@@ -0,0 +1,24 @@
+package com.naosteam.watchvideoapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.naosteam.watchvideoapp.R;
+
+
+public class VideoDetailFragment extends Fragment {
+
+    private View rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_video_detail, container, false);
+        return rootView;
+    }
+}
--- app/src/main/java/com/naosteam/watchvideoapp/fragments/VideoFragment.java
+++ app/src/main/java/com/naosteam/watchvideoapp/fragments/VideoFragment.java
@@ -3,64 +3,38 @@ package com.naosteam.watchvideoapp.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.naosteam.watchvideoapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link VideoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class VideoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View rootView;
+    private NavController navController;
+    private TextView tv_video;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public VideoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VideoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VideoFragment newInstance(String param1, String param2) {
-        VideoFragment fragment = new VideoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video, container, false);
+
+        rootView = inflater.inflate(R.layout.fragment_video, container, false);
+
+        navController = NavHostFragment.findNavController(this);
+
+        tv_video = rootView.findViewById(R.id.tv_video);
+        tv_video.setOnClickListener(v->{
+            navController.navigate(R.id.VideoToDetail);
+        });
+
+
+        return rootView;
     }
 }
