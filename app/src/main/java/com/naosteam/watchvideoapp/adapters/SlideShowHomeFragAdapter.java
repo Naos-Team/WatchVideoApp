134e653ca68994a2c5b9d2b4358481cfa78e6012
yennhiluu2112 <yennhiluu2112@gmail.com>
2022-05-07 14:03:36 +0700

    Tuyen commit: wait Raido Adapter & detail screen

--- app/src/main/java/com/naosteam/watchvideoapp/adapters/SlideShowHomeFragAdapter.java
+++ app/src/main/java/com/naosteam/watchvideoapp/adapters/SlideShowHomeFragAdapter.java
@@ -1,30 +1,26 @@
 package com.naosteam.watchvideoapp.adapters;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.naosteam.watchvideoapp.R;
-import com.naosteam.watchvideoapp.listeners.SlideClickListeners;
+import com.naosteam.watchvideoapp.listeners.OnHomeItemClickListeners;
 import com.naosteam.watchvideoapp.models.Category_M;
 import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
 
 import java.util.ArrayList;
-import java.util.zip.Inflater;
 
 public class SlideShowHomeFragAdapter extends RecyclerView.Adapter<SlideShowHomeFragAdapter.SlideShowHomeHolder> {
     private ArrayList<Category_M> category_mArrayList;
-    private SlideClickListeners listeners;
+    private OnHomeItemClickListeners listeners;
 
-    public SlideShowHomeFragAdapter(ArrayList<Category_M> category_mArrayList, SlideClickListeners listeners) {
+    public SlideShowHomeFragAdapter(ArrayList<Category_M> category_mArrayList, OnHomeItemClickListeners listeners) {
         this.category_mArrayList = category_mArrayList;
         this.listeners = listeners;
     }
@@ -49,26 +45,20 @@ public class SlideShowHomeFragAdapter extends RecyclerView.Adapter<SlideShowHome
 
     class SlideShowHomeHolder extends RecyclerView.ViewHolder{
         ImageView imgSlide_home_frag_item;
+        ConstraintLayout layout_item_Slide_item_frag_home;
         public SlideShowHomeHolder(@NonNull View itemView) {
             super(itemView);
             imgSlide_home_frag_item = (ImageView) itemView.findViewById(R.id.imgSlide_home_frag_item);
+            layout_item_Slide_item_frag_home = (ConstraintLayout)
+                    itemView.findViewById(R.id.layout_item_Slide_item_frag_home);
         }
 
         public void bindView(int position){
-            Picasso.get().load(category_mArrayList.get(position).getCat_image()).into(new Target() {
+            Picasso.get().load(category_mArrayList.get(position).getCat_image()).into(imgSlide_home_frag_item);
+            layout_item_Slide_item_frag_home.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                    imgSlide_home_frag_item.setBackground(new BitmapDrawable(bitmap));
-                }
-
-                @Override
-                public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-
-                }
-
-                @Override
-                public void onPrepareLoad(Drawable placeHolderDrawable) {
-
+                public void onClick(View view) {
+                    listeners.onClick_SlideShow(position);
                 }
             });
         }
