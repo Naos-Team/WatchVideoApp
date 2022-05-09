9c31e7d12576651ad1ec5825a074a0a2ebce9b9b
yennhiluu2112 <yennhiluu2112@gmail.com>
2022-05-09 21:47:39 +0700

    log out

--- app/src/main/java/com/naosteam/watchvideoapp/fragments/MoreFragment.java
+++ app/src/main/java/com/naosteam/watchvideoapp/fragments/MoreFragment.java
@@ -3,64 +3,49 @@ package com.naosteam.watchvideoapp.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.naosteam.watchvideoapp.R;
+import com.naosteam.watchvideoapp.databinding.FragmentMoreBinding;
+import com.naosteam.watchvideoapp.databinding.FragmentRadioBinding;
+import com.naosteam.watchvideoapp.utils.Constant;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MoreFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MoreFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MoreFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MoreFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MoreFragment newInstance(String param1, String param2) {
-        MoreFragment fragment = new MoreFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private View rootView;
+    private NavController navController;
+    private FragmentMoreBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        binding = FragmentMoreBinding.inflate(inflater, container, false);
+        rootView = binding.getRoot();
+        navController = NavHostFragment.findNavController(this);
+
+        LogOut();
+
+        return rootView;
+    }
+
+    private void LogOut(){
+        binding.constraintlayout25.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (FirebaseAuth.getInstance().getCurrentUser()!=null){
+                    FirebaseAuth.getInstance().signOut();
+                    if (Constant.ggclient!=null){
+                        Constant.ggclient.signOut();
+                    }
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/naosteam/watchvideoapp/utils/Constant.java
+++ app/src/main/java/com/naosteam/watchvideoapp/utils/Constant.java
@@ -1,9 +1,11 @@
 package com.naosteam.watchvideoapp.utils;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.naosteam.watchvideoapp.models.Videos_M;
 
 public class Constant {
     public static final String SERVER_URL = "https://musicfreeworld.com/naosteam/watchvideoapp/";
     public static final String ERR_TAG = "ERROR";
     public static Videos_M Radio_Listening = new Videos_M(-1,-1,"Radio Name","", "","", 0,0,0,false,null );
+    public static GoogleSignInClient ggclient;
 }
