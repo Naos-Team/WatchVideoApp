ce6c9474e66159a2dbca8e7a4aba8d7a65d88c7c
yennhiluu2112 <yennhiluu2112@gmail.com>
2022-05-11 23:23:56 +0700

    Tuyen commit: fix radio play in end

--- app/src/main/java/com/naosteam/watchvideoapp/fragments/RadioDetailsFragment.java
+++ app/src/main/java/com/naosteam/watchvideoapp/fragments/RadioDetailsFragment.java
@@ -58,7 +58,7 @@ public class RadioDetailsFragment extends Fragment {
             public void onEnd() {
                 controlRadioListener.onNext();
                 updateView();
-                playerRadio.playRadio();
+                playerRadio.startRadio();
             }
         });
         LoadData();
